e93b5577ec512a08920b7ee5743b0dc6ef025bcd
Jake Rieger <contact.jakerieger@gmail.com>
2024-12-22 18:50:50 -0500

    fixed incorrect attribute initialization in OpenGL

--- Modules/Graphics/VertexArray.cpp
+++ Modules/Graphics/VertexArray.cpp
@@ -4,63 +4,61 @@
 
 #include "VertexArray.hpp"
 
-namespace x {
-    namespace Graphics {
-        VertexArray::VertexArray(const std::vector<VertexAttribute>& attributes,
-                                 const std::vector<f32>& vertices,
-                                 const std::vector<u32>& indices) {
-            _attributes   = attributes;
-            _vertexBuffer = std::make_unique<Memory::VertexBuffer>(vertices);
-            _indexBuffer  = std::make_unique<Memory::IndexBuffer>(indices);
-            glGenVertexArrays(1, &_vao);
-            if (_vao == 0) { Panic("Vertex Array creation failed"); }
-            for (const auto& attribute : _attributes) {
-                enableAttribute(attribute);
-            }
+namespace x::Graphics {
+    VertexArray::VertexArray(const std::vector<VertexAttribute>& attributes,
+                             const std::vector<f32>& vertices,
+                             const std::vector<u32>& indices) {
+        _attributes   = attributes;
+        _vertexBuffer = std::make_unique<Memory::VertexBuffer>(vertices);
+        _indexBuffer  = std::make_unique<Memory::IndexBuffer>(indices);
+        glGenVertexArrays(1, &_vao);
+        if (_vao == 0) { Panic("Vertex Array creation failed"); }
+        for (const auto& attribute : _attributes) {
+            enableAttribute(attribute);
         }
+    }
 
-        VertexArray::~VertexArray() {
-            glDeleteVertexArrays(1, &_vao);
-        }
+    VertexArray::~VertexArray() {
+        glDeleteVertexArrays(1, &_vao);
+    }
 
-        void VertexArray::bind() const {
-            glBindVertexArray(_vao);
-        }
+    void VertexArray::bind() const {
+        glBindVertexArray(_vao);
+    }
 
-        void VertexArray::enableAttribute(const VertexAttribute& attr) const {
-            bindVertex();
-            bind();
-            glEnableVertexAttribArray(attr.index);
-            glVertexAttribPointer(attr.index,
-                                  attr.size,
-                                  attr.type,
-                                  attr.normalized,
-                                  attr.size,
-                                  attr.offset);
-        }
+    void VertexArray::enableAttribute(const VertexAttribute& attr) const {
+        bindVertex();
+        bind();
+        glEnableVertexAttribArray(attr.index);
+        glVertexAttribPointer(attr.index,
+                              attr.size,
+                              attr.type,
+                              attr.normalized,
+                              attr.stride,
+                              attr.offset);
+    }
 
-        void VertexArray::disableAttribute(const VertexAttribute& attr) const {
-            bindVertex();
-            bind();
-            glDisableVertexAttribArray(attr.index);
-        }
+    void VertexArray::disableAttribute(const VertexAttribute& attr) const {
+        bindVertex();
+        bind();
+        glDisableVertexAttribArray(attr.index);
+    }
 
-        void VertexArray::unbind() {
-            glBindVertexArray(0);
-        }
+    void VertexArray::unbind() {
+        glBindVertexArray(0);
+    }
 
-        void VertexArray::unbindBuffers() {
-            glBindBuffer(GL_ARRAY_BUFFER, 0);
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-            unbind();
-        }
+    void VertexArray::unbindBuffers() {
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        unbind();
+    }
 
-        void VertexArray::bindVertex() const {
-            _vertexBuffer->bind();
-        }
+    void VertexArray::bindVertex() const {
+        _vertexBuffer->bind();
+    }
 
-        void VertexArray::bindIndex() const {
-            _indexBuffer->bind();
-        }
-    }  // namespace Graphics
-}  // namespace x
+    void VertexArray::bindIndex() const {
+        _indexBuffer->bind();
+    }
+}  // namespace x::Graphics
